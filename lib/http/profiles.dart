d9c385ef02d4abe638811818253d47b9197c7a6f
omeadrr <omeadr@yahoo.com>
2020-09-01 17:36:27 +0430

    10.6.98

---
+++ lib/http/profiles.dart
@@ -0,0 +1,19 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:stubbbb/mahde/profileModels.dart';
+
+class ProfileHttp {
+  static Future<Map> getData() async {
+    List<Models> profiles = [];
+    var response = await http.get('http://192.168.56.1/Stub/userGetData.php');
+
+    var responseBody = json.decode(response.body);
+    responseBody.forEach((item) {
+      profiles.add(Models.fromjson(item));
+    });
+    return {
+      'profiles' : profiles
+      };
+  }
+}
