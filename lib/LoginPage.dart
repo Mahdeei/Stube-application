4255f3e22069091ed5d95b7b535a29efdc0bd10b
mahdee <khmahdee@gmail.com>
2020-09-02 10:52:08 +0430

    edit qadimi

--- lib/LoginPage.dart
+++ lib/LoginPage.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:stubbbb/mahde/home.dart';
+import 'package:dio/dio.dart';
+import 'dart:convert';
+import 'dart:io';
+
 
 class LoginPage extends StatefulWidget {
   @override
@@ -7,24 +11,33 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+
+  bool isLoading = false;
+  String message="";
+  TextEditingController _controllerUser,_controllerPassword;
+
+  @override
+  void initState() {
+    _controllerUser = new TextEditingController();
+    _controllerPassword = new TextEditingController();
+  }
+
+
+
+
+
   @override
   Widget build(BuildContext context) {
     var phonesize = MediaQuery.of(context).size;
-    return new MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home: new Directionality(
+
+    return new Directionality(
             textDirection: TextDirection.rtl,
             child: new SafeArea(
               top: true,
               child: new Scaffold(
                 body: new Stack(
                   children: <Widget>[
-                    new Container(color: Colors.white,
-                      height: phonesize.height,
-                      width: phonesize.width,
-                      child: new Align(alignment: Alignment.bottomLeft,
-                          child: new Opacity(opacity: 0.25,
-                              child: new Image(image: AssetImage('assets/image/hom.png',),height: phonesize.height*0.4,))),),
+                    ContainerImage(phonesize),
                     new ClipPath(
                       clipper: ClippLogin(),
                       child:Container(
@@ -38,7 +51,9 @@ class _LoginPageState extends State<LoginPage> {
                               ),
                             new SizedBox(height: phonesize.height*0.05,),
                             new Padding(padding: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 30.0),
-                              child: TextFormField(cursorColor: Colors.white,
+                              child: TextFormField(
+                                controller: _controllerUser,
+                                cursorColor: Colors.white,
                                 decoration: InputDecoration(prefixIcon: Icon(Icons.school,color: Colors.white,),
                                     labelText: 'نام کاربری',
                                     labelStyle: TextStyle(color: Colors.white),
@@ -51,7 +66,9 @@ class _LoginPageState extends State<LoginPage> {
 
                               ),),
                             new Padding(padding: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 30.0),
-                              child: TextFormField(cursorColor: Colors.white,
+                              child: TextFormField(
+                                controller: _controllerPassword,
+                                cursorColor: Colors.white,
                                 decoration: InputDecoration(prefixIcon: Icon(Icons.lock,color: Colors.white,),
                                     labelText: 'کلمه عبور',
                                     labelStyle: TextStyle(color: Colors.white),
@@ -70,17 +87,27 @@ class _LoginPageState extends State<LoginPage> {
                     new Positioned(top: phonesize.height*0.6-37,
                         right: phonesize.width*9/16,
                         child: new GestureDetector(
-                          onTap: ()=> Navigator.push(context, MaterialPageRoute(builder: (context) => new Directionality(textDirection: TextDirection.rtl, child: HomePage()))),
-                          child: new Container(decoration: BoxDecoration(boxShadow: [BoxShadow( color: Colors.black,
-                              offset: Offset(2,2),
-                              blurRadius: 7)],
-                              color: Color(0xffFE346E),
-                              borderRadius: BorderRadius.circular(30.0)
+                          onTap: ()=>
+                            getData(),
+//                              Navigator.push(
+//                                  context,
+//                                  MaterialPageRoute(
+//                                      builder: (context) =>
+//                                      new Directionality(
+//                                          textDirection: TextDirection.rtl,
+//                                          child: HomePage()))),
+                          child: new Container(
+                            decoration: BoxDecoration(
+                                boxShadow: [BoxShadow( color: Colors.black,
+                                  offset: Offset(2,2),
+                                  blurRadius: 7)],
+                                  color: Color(0xffFE346E),
+                                borderRadius: BorderRadius.circular(30.0)
                           ),
-    //                    height: 20.0,
-
                             child: new Padding(padding: const EdgeInsets.symmetric(horizontal: 35.0,vertical: 0),
-                              child: new Text('ورود',style: TextStyle(fontSize:25,color: Colors.white),textAlign: TextAlign.center,),),
+                              child: isLoading
+                                  ? new CircularProgressIndicator()
+                                  :new Text('ورود',style: TextStyle(fontSize:25,color: Colors.white),textAlign: TextAlign.center,),),
                           ),
                         )),
                     new Positioned(top: phonesize.height*0.6-37,
@@ -97,12 +124,58 @@ class _LoginPageState extends State<LoginPage> {
     //                    height: 20.0,
 
                             child: new Padding(padding: const EdgeInsets.symmetric(horizontal: 25.0,vertical: 0),
-                              child: new Text('ثبت نام',style: TextStyle(fontSize:25,color: Color(0xff2C003E)),textAlign: TextAlign.center,),),
+                              child: new Text(
+                                    'ثبت نام',
+                                    style: TextStyle(fontSize:25,color: Color(0xff2C003E)),textAlign: TextAlign.center,),),
                           ),
                         )),
                   ],
                 )
-        ))));
+        )));
+  }
+
+  getData() async {
+    setState(() {
+      isLoading = true;
+    });
+
+    Dio dio = new Dio();
+    Response response = await dio.post('http://192.168.1.7/dbStub/',
+        data: new FormData.fromMap({
+          "username": _controllerUser,
+          "password": _controllerPassword,
+        }),
+        options: new Options(contentType: "application/x-www-form-urlencoded"));
+
+    try{
+      print(response.data);
+      final data = json.decode(response.data);
+      if(data['server'] == 'connect seccessful'){
+        if(data['status']=='data sent is correct'){
+          // Navigator.of(context).pushReplacement(
+          //     new MaterialPageRoute(builder: (ctx) => )
+          // );
+        }else{
+          setState(() {
+            isLoading = false;
+            message = 'Please Enter correct';
+          });
+        }
+      }else{
+        setState(() {
+          message = 'Error in DataBase';
+        });
+      }
+    }catch(e){
+      print(e);
+      setState(() {
+        isLoading = false;
+        message = 'have a Exception';
+      });
+
+    }
+
+
   }
 }
 
@@ -129,7 +202,14 @@ class ClippLogin extends CustomClipper<Path>{
 
 }
 
-
+Widget ContainerImage(var phonesize){
+  return new Container(color: Colors.white,
+    height: phonesize.height,
+    width: phonesize.width,
+    child: new Align(alignment: Alignment.bottomLeft,
+        child: new Opacity(opacity: 0.25,
+            child: new Image(image: AssetImage('assets/image/hom.png',),height: phonesize.height*0.4,))),);
+}
 
 
 
